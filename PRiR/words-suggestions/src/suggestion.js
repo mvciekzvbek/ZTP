73e8b5779131763cc799f7c091f733d1f110559e
mvciekzvbek <maciej.jan.zabek@gmail.com>
2018-06-06 22:36:28 +0200

    working version

--- PRiR/words-suggestions/src/suggestion.js
+++ PRiR/words-suggestions/src/suggestion.js
@@ -8,9 +8,12 @@ var suggestion = (function () {
 		2: [],
 		3: []
 	};
+	var workers = [];
+	var found = false;
 
 	var textarea = $('#box');
 	var resultBox = $('#result');
+	var start;
 
 	var init = function () {
 		prepareThreads(4);
@@ -20,6 +23,7 @@ var suggestion = (function () {
 
 			var target = (typed[typed.length - 1] !== '' || typed[typed.length - 1].length < 2 ? typed[typed.length - 1] : typed[typed.length - 2]);
 
+			start = Date.now();
 			findResult(target);
 		});
 	};
@@ -28,52 +32,78 @@ var suggestion = (function () {
 		return URL.createObjectURL(new Blob([`(${f.toString()})()`], { type: 'application/javascript' }));
 	};
 
-	var prepareThreads = function (threads) {
+	var prepareThreads = function () {
 		$.ajax({
 			url: 'slowa.txt',
 			async: true,
 			success: function (data) {
 				var lines = data.split('\n');
-				for (var i = 0, n = data.length; i < n; ++i) {
+				for (var i = 0, n = lines.length; i < n; ++i) {
+					var line = lines[i].slice(0, -1);
 					if (i % threads === 0) {
-						words[0].push(lines[i]);
+						words[0].push(line);
 					} else if (i % threads === 1) {
-						words[1].push(lines[i]);
+						words[1].push(line);
 					} else if (i % threads === 2) {
-						words[2].push(lines[i]);
+						words[2].push(line);
 					} else if (i % threads === 3) {
-						words[3].push(lines[i]);
+						words[3].push(line);
 					}
+				}
+
+				var workerBlob = hackFunctionToBuffer(workerFunction);
+
+				for (var j = 0; j < threads; j++) {
+					var worker = new Worker(workerBlob);
+					workers.push(worker);
+
+					// send just an array
+					worker.postMessage({
+						content: 'array',
+						words: words[j]
+					});
 				}
 			}
 		});
 	};
 
 	var findResult = function (target) {
-		var workerBlob = hackFunctionToBuffer(workerFunction);
-		var result = [];
+		var result = '';
 
-		for (let j = 0; j < threads; j++) {
-			var worker = new Worker(workerBlob);
+		for (var j = 0, n = workers.length; j < n; j++) {
+			var worker = workers[j];
+
+			worker.postMessage({
+				content: 'target',
+				word: target
+			});
 
 			// recv
 			worker.onmessage = function (message) {
-				result.push(message.data);
-				resultBox.html(result[0]);
-				this.terminate();
-			};
+				var word = message.data.word;
+				var distance = message.data.distance;
+				var min = 99;
 
-			// send 
-			worker.postMessage(
-				{
-					words: words[j],
-					target: target
+				if (!found) {
+					if (word === target) {
+						result = word;
+						resultBox.html(result + ', ' + (Date.now() - start) / 1000);
+						found = true;
+						return;
+					} else {
+						if (distance < min) {
+							min = distance;
+							result = word;
+							resultBox.html(result + ', ' + (Date.now() - start) / 1000);
+						}
+					}
 				}
-			);
-		}
+			};
+		};
 	};
 
 	var workerFunction = function () {
+		var words = [];
 		/**
 		 * @param {string} a
 		 * @param {string} b
@@ -90,22 +120,22 @@ var suggestion = (function () {
 			// Fill the first row of the matrix.
 			// If this is first row then we're transforming empty string to a.
 			// In this case the number of transformations equals to size of a substring.
-			for (let i = 0; i <= a.length; i += 1) {
+			for (var i = 0; i <= a.length; i += 1) {
 				distanceMatrix[0][i] = i;
 			}
 
 			// Fill the first column of the matrix.
 			// If this is first column then we're transforming empty string to b.
 			// In this case the number of transformations equals to size of b substring.
-			for (let j = 0; j <= b.length; j += 1) {
+			for (var j = 0; j <= b.length; j += 1) {
 				distanceMatrix[j][0] = j;
 			}
 
-			for (let j = 1; j <= b.length; j += 1) {
-				for (let i = 1; i <= a.length; i += 1) {
+			for (var j = 1; j <= b.length; j += 1) {
+				for (var i = 1; i <= a.length; i += 1) {
 					const indicator = a[i - 1] === b[j - 1] ? 0 : 1;
 					distanceMatrix[j][i] = Math.min(
-						distanceMatrix[j][i - 1] + 1, // deletion
+						distanceMatrix[j][i - 1] + 1, // devarion
 						distanceMatrix[j - 1][i] + 1, // insertion
 						distanceMatrix[j - 1][i - 1] + indicator // substitution
 					);
@@ -114,26 +144,34 @@ var suggestion = (function () {
 			return distanceMatrix[b.length][a.length];
 		};
 
-		this.onmessage = function (message) {
-			var words = message.data.words;
-			let target = message.data.target;
-			let minimumDistance = 9999999;
-			let word = '';
-
-			for (let i = 0; i < words.length; i++) {
-				let distance = levenshteinDistance(target, words[i]);
-				if (distance === 0) {
-					word = words[i];
+		this.onmessage = function (e) {
+			var msg = e.data;
+			switch (msg.content) {
+				case 'array':
+					words = msg.words;
 					break;
-				} else if (distance < minimumDistance) {
-					minimumDistance = distance;
-					word = words[i];
-				} else {
-					word = 'Error';
-				}
-			}
+				case 'target':
+					var target = msg.word;
+					var minimumDistance = 9999999;
+					var word = '';
 
-			this.postMessage(word);
+					for (var i = 0, n = words.length; i < n; i++) {
+						var distance = levenshteinDistance(target, words[i]);
+						if (distance === 0) {
+							word = words[i];
+							minimumDistance = 0;
+							break;
+						} else if (distance < minimumDistance) {
+							minimumDistance = distance;
+							word = words[i];
+						}
+					}
+					this.postMessage({
+						word,
+						distance: minimumDistance
+					});
+					break;
+			}
 		};
 	};
 
