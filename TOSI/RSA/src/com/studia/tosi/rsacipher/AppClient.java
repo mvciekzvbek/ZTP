b12d02b26d24a6b681a429e85a8109f5f0f8ab3f
Maciej Ząbek <maciek@maciek.local>
2018-12-18 20:45:08 +0100

    bouncy castle

--- TOSI/RSA/src/com/studia/tosi/rsacipher/AppClient.java
+++ TOSI/RSA/src/com/studia/tosi/rsacipher/AppClient.java
@@ -2,9 +2,7 @@ package com.studia.tosi.rsacipher;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.math.BigInteger;
 import java.util.List;
 
@@ -13,9 +11,20 @@ public class AppClient {
     public static byte[] extractBytes(String filename) throws IOException {
         BufferedImage bufferimage = ImageIO.read(new File(filename));
         ByteArrayOutputStream output = new ByteArrayOutputStream();
-        ImageIO.write(bufferimage, "jpg", output );
-        byte [] data = output.toByteArray();
-        return data;
+        ImageIO.write(bufferimage, "jpg", output);
+
+        byte[] bytes = output.toByteArray();
+//        for(byte b : bytes) {
+//            System.out.println(b);
+//        }
+        return bytes;
+    }
+
+
+    public static void createImageFromBytes(byte[] imageBytes) throws IOException {
+        try (FileOutputStream fos = new FileOutputStream("output.jpg")) {
+            fos.write(imageBytes);
+        }
     }
 
     public static void main(String[] args) {
@@ -25,15 +34,17 @@ public class AppClient {
             RsaCipher rsa = new RsaCipher();
             List<BigInteger> encrypted = rsa.encrypt(imageBytes);
 
-            System.out.println("Encrypted image bytes: ");
-            // print image bytes
+//            System.out.println("Encrypted image bytes: ");
+//            // print image bytes
+//            for (BigInteger el : encrypted) {
+//                System.out.println(el.toString());
+//            }
 
-            // byte[] decrypted = rsa.decrypt(encrypted);
+            byte[] decrypted = rsa.decrypt(encrypted);
 
-            // save file
+            createImageFromBytes(decrypted);
         } catch (Exception e) {
             System.out.println(e);
         }
-
     }
 }
--- TOSI/RSA/src/com/studia/tosi/rsacipher/RsaCipher.java
+++ TOSI/RSA/src/com/studia/tosi/rsacipher/RsaCipher.java
@@ -5,21 +5,25 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import org.bouncycastle.util.BigIntegers;
 
 class RsaCipher {
     private PublicKey publicKey;
     private PrivateKey privateKey;
+    private BigInteger one = new BigInteger("1");
+    private BigInteger three = new BigInteger("3");
+    private BigInteger four = new BigInteger("4");
+    // n is 2048bit size which is 256bytes  => m < n => max size is 255 bytes
+    private int size = 255;
 
     private BigInteger[] generatePrimes() {
         BigInteger[] primes = new BigInteger[2];
-        BigInteger three = new BigInteger("3");
-        BigInteger four = new BigInteger("4");
         BigInteger p, q;
 
         do {
             Random rnd = new Random();
-            p = new BigInteger(1024, 1, rnd);
-            q = new BigInteger(1024, 1, rnd);
+            p = new BigInteger(1024, 3000, rnd);
+            q = new BigInteger(1024, 3000, rnd);
         } while (p.mod(four).equals(three) && q.mod(four).equals(three));
 
         primes[0] = p;
@@ -39,7 +43,7 @@ class RsaCipher {
         BigInteger p = primes[0];
         BigInteger q = primes[1];
 
-        return (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));
+        return (p.subtract(one)).multiply(q.subtract(one));
     }
 
     private PublicKey createPublicKey (BigInteger n, BigInteger e) {
@@ -53,39 +57,35 @@ class RsaCipher {
 
     private BigInteger calculateEuler(BigInteger fi) {
         BigInteger euler;
+
         do {
             Random rnd = new Random();
-            euler = new BigInteger(fi.bitLength(), rnd);
-        } while (euler.compareTo(fi.subtract(BigInteger.ONE)) < 0 && euler.compareTo(BigInteger.ONE) > 0 && euler.gcd(fi).equals(BigInteger.ONE));
+            euler = BigInteger.probablePrime(fi.bitLength(), rnd);
+        } while (euler.compareTo(one) > 0 && euler.compareTo(fi.subtract(one)) < 0 && (euler.gcd(fi)).equals(one));
 
         return euler;
     }
 
     private BigInteger encryptPart(byte[] part, PublicKey publicKey) {
-        BigInteger biPart = new BigInteger(part);
+        BigInteger biPart = new BigInteger(1, part);
         return biPart.modPow(publicKey.n, publicKey.e);
     }
 
-    private List<BigInteger> encryptImageBytes(byte[] imageBytes) {
-        // n is 2048bit size which is 256bytes  => m < n => max size is 255 bytes
-        int size = 255;
+    private BigInteger decryptPart(BigInteger part) {
+        return part.modPow(this.privateKey.d, this.privateKey.n);
+    }
 
-        List<byte[]> devidedByteArray = divideByteArray(imageBytes, size);
+    private List<BigInteger> encryptImageBytes(byte[] imageBytes) {
+        List<byte[]> devidedByteArray = divideByteArray(imageBytes, this.size);
 
         List<BigInteger> bigIntegers = new ArrayList<>();
 
         for (byte[] part : devidedByteArray) {
-            System.out.println("part");
             bigIntegers.add(encryptPart(part, this.publicKey));
         }
 
         return bigIntegers;
     }
-
-//    private byte[] decryptImage(List<BigInteger> encrypted) {
-//
-//
-//    }
 
     public static List<byte[]> divideByteArray(byte[] source, int chunksize) {
         List<byte[]> result = new ArrayList<>();
@@ -114,7 +114,33 @@ class RsaCipher {
         return encryptImageBytes(imageBytes);
     }
 
-//    public byte[] decrypt (List<BigInteger> encrypted) {
-//        return decryptImage(encrypted);
-//    }
+    public byte[] decrypt (List<BigInteger> encrypted) {
+
+        List<BigInteger> decrypted = new ArrayList<>();
+
+        for (BigInteger part : encrypted) {
+            decrypted.add(decryptPart(part));
+        }
+
+        List <Byte> bytes = new ArrayList<Byte>();
+
+        for (int i = 0, j = decrypted.size(); i < j; i++) {
+            BigInteger part = decrypted.get(i);
+            byte[] partAsBytes = BigIntegers.asUnsignedByteArray(part);
+
+            for(byte b : partAsBytes) {
+                bytes.add(b);
+            }
+        }
+
+        return convertBytes(bytes);
+    }
+
+    private byte[] convertBytes(List<Byte> bytes) {
+        byte[] ret = new byte[bytes.size()];
+        for (int i = 0; i < ret.length; i++) {
+            ret[i] = bytes.get(i);
+        }
+        return ret;
+    }
 }
